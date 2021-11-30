a31dc36b2f31a8d5a32d896c82df454e8c09fb42
Nikaree <ri_v@mail.ru>
2021-12-01 00:46:31 +1000

    npm

---
+++ npm/index.js
@@ -0,0 +1,10 @@
+var colors = require('colors/safe');
+console.log(colors.black('Hello'));
+console.log(colors.red('My'));
+console.log(colors.green('Name'));
+console.log(colors.yellow('is'));
+console.log(colors.blue('MiSun (Korean name)'));
+console.log(colors.magenta('And'));
+console.log(colors.cyan('Friends'));
+console.log(colors.white('Call me'));
+console.log(colors.gray('Nika (Russian name)'));
