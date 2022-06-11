f93ebd4ad64b3dbd7cd6445566ddd7c335d2b03d
Nikaree <ri_v@mail.ru>
2022-06-11 17:14:30 +1000

    ctm

---
+++ composer/twig/index.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once __DIR__ . '/bootstrap.php';
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+$logger = new Logger('main');
+$logger->pushHandler(new StreamHandler(__DIR__ . '/logs/app.log', Logger::DEBUG));
+
+class First{
+  public function writeText(){
+    return "Class 1";
+  }
+  public function wholeText(){
+    return "Text of 1 class which will be in others";
+  }
+}
+
+class Second extends First{
+  public function writeText(){
+    return "Class 2";
+  }
+}
+
+class Third extends Second{
+  public function writeText(){
+    return "Class 3";
+  }
+}
+
+$firstClass =  new First();
+$firstClassText = $firstClass->writeText();
+$firstClassWholeText = $firstClass->wholeText();
+$secondClass =  new Second();
+$secondClassText = $secondClass->writeText();
+$secondClassWholeText = $secondClass->wholeText();
+$thirdClass =  new Third();
+$thirdClassText = $thirdClass->writeText();
+$thirdClassWholeText = $thirdClass->wholeText();
+
+$info = [
+  ['name' => $firstClassText],
+  ['name' => $firstClassWholeText],
+  ['name' => $secondClassText],
+  ['name' => $secondClassWholeText],
+  ['name' => $thirdClassText],
+  ['name' => $thirdClassWholeText],
+];
+
+// Render our view
+echo $twig->render('index.html.twig', ['info' => $info]);
+$logger->info('New http request');
+$logger->info('index.php');
